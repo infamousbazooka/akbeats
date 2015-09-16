b4bb39b5ef14daa310b4db7c2cbad7173e3f1085
infamousbazooka <magnusfernandes1295@gmail.com>
2015-09-16 21:56:00 +0530

    Bootstrap removed and Instruments added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('.section').css('height', $(window).height() + "px");
+	$('.section-half').css('height', ($(window).height()/2) + "px");
 	$('.main').css('line-height', $(window).height() + "px");
 });
 $(window).resize(function() {
