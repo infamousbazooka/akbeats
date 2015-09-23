1d71cdc11e192f7ca009afbc3753bf021326a0f4
infamousbazooka <magnusfernandes1295@gmail.com>
2015-09-23 18:36:51 +0530

    Arranged the instruments

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
 	$('.section').css('height', $(window).height() + "px");
-	$('.section-half').css('height', ($(window).height()/2) + "px");
+	$('.section-half').css('height', ($(window).height()*1/4) + "px");
+	$('.three-inst').css('height', ($(window).height()*3/4) + "px");
 	$('.main').css('line-height', $(window).height() + "px");
 });
 $(window).resize(function() {
 	$('.section').css('height', $(window).height() + "px");
 	$('.main').css('line-height', $(window).height() + "px");
 });
+
+function player (instr) {
+	var audio = $(instr)[0];
+	audio.play();
+}
