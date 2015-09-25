1e3c3e90318431d6d41342f322ec16c68d6fd190
infamousbazooka <magnusfernandes1295@gmail.com>
2015-09-25 16:23:49 +0530

    Contact Template complete

---
+++ Gallery/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$('.section').css('min-height', $(window).height() + "px");
+	$('.main').css('line-height', $(window).height() + "px");
+});
+$(window).resize(function() {
+	$('.section').css('min-height', $(window).height() + "px");
+	$('.main').css('line-height', $(window).height() + "px");
+});
+var num = $(window).height();
+$(window).bind('scroll', function() {
+	if ($(window).scrollTop() > num) {
+		$('.menu').addClass('fixed');
+	}
+	else {
+		num = $(window).height();
+		$('.menu').removeClass('fixed');
+	}
+});
