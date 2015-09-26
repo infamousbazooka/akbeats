3036147b11e3b720aec7eaf288ae5f812c4128b6
infamousbazooka <magnusfernandes1295@gmail.com>
2015-09-27 00:13:25 +0530

    Admin Structure

---
+++ admin/js/main.js
@@ -0,0 +1,21 @@
+function hideloader(){
+	$('.loader').css('display', 'none');
+}
+$(document).ready(function() {
+	$('.section').css('min-height', $(window).height() + "px");
+	$('.main').css('line-height', $(window).height() + "px");
+});
+$(window).resize(function() {
+	$('.section').css('min-height', $(window).height() + "px");
+	$('.main').css('line-height', $(window).height() + "px");
+});
+var num = $(window).height();
+$(window).bind('scroll', function() {
+	if ($(window).scrollTop() > num) {
+		$('.menu').addClass('fixed');
+	}
+	else {
+		num = $(window).height();
+		$('.menu').removeClass('fixed');
+	}
+});
