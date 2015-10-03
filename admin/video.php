af3eeebefd471c8f45fc5882b692a40c1e83c5a5
infamousbazooka <magnusfernandes1295@gmail.com>
2015-10-04 02:56:59 +0530

    Canvas Added

---
+++ admin/video.php
@@ -0,0 +1,44 @@
+<?php
+
+$target_dir = "../Gallery/Videos/videos/";
+$target_file = $target_dir . basename($_FILES["fileToUpload1"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload1"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["upload"]["size"] > 50000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "mp4" && $imageFileType != "avi") {
+    echo "Sorry, only mp4 files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload1"]["tmp_name"], "../Gallery/Videos/videos/" . $filecount . ".jpg")) {
+        echo "The file ". basename( $_FILES["fileToUpload1"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
